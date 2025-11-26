1d277f64ac483d3bc343bb37ecd218012d4e4b2c
Andrew Bennecke <jandrew624@hotmail.com>
2025-11-26 06:37:57 -0800

    Updated scripts

--- .ipynb_checkpoints/spine_reg_pipeline-checkpoint.py
+++ .ipynb_checkpoints/spine_reg_pipeline-checkpoint.py
@@ -1,1 +1,19 @@
 import argparse
+
+
+def main():
+    """
+    Command Line Arguments:
+    =======================
+
+    Raises:
+    =======
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('fname_I', type=str, help = 'The path to the interpolated atlas .npz file')
+    parser.add_argument('fname_L', type=str, help = 'The path to the interpolated atlas labels .npz file')
+    parser.add_argument('fname_J', type=str, help = 'The path to the spine reflection .npz file')
+    parser.add_argument('fname_pointJ', type=str, help = 'The input .swc file used for more precise registration')
+    
