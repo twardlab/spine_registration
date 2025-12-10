32f36243ce578f7ab9a9f3897731095d833ef1b7
Andrew Bennecke <jandrew624@hotmail.com>
2025-12-10 13:29:29 -0800

    Moved notebooks and scripts folders to root dir

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 import sys
 sys.path.append('/home/runner/work/spine_registration/spine_registration/docs')
-sys.path.append('/home/runner/work/spine_registration/spine_registration/docs/notebooks')
-sys.path.append('/home/runner/work/spine_registration/spine_registration/docs/scripts')
+sys.path.append('/home/runner/work/spine_registration/spine_registration/notebooks')
+sys.path.append('/home/runner/work/spine_registration/spine_registration/scripts')
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
