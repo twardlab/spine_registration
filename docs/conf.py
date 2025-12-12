6394db5b32514cd4a1d5c46730fcc8c5cb946b86
Andrew Bennecke <jandrew624@hotmail.com>
2025-12-12 08:07:52 -0800

    debugging new dir

--- docs/conf.py
+++ docs/conf.py
@@ -34,12 +34,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 import sys
 import os
+from pathlib import Path
 # sys.path.append('/home/runner/work/spine_registration/spine_registration/docs')
 # sys.path.append('/home/runner/work/spine_registration/spine_registration/docs/notebooks')
 # sys.path.append('/home/runner/work/spine_registration/spine_registration/docs/scripts')
-sys.path.insert(0, os.path.abspath("../source/"))
-sys.path.insert(0, os.path.abspath("../source/notebooks"))
-sys.path.insert(0, os.path.abspath("../source/scripts"))
+# sys.path.insert(0, os.path.abspath("../source/"))
+# sys.path.insert(0, os.path.abspath("../source/notebooks"))
+# sys.path.insert(0, os.path.abspath("../source/scripts"))
+sys.path.insert(0, str(Path('..', 'source').resolve()))
+sys.path.insert(0, str(Path('..', 'source', 'notebooks').resolve()))
+sys.path.insert(0, str(Path('..', 'source', 'scripts').resolve()))
 
 
 # -- Options for HTML output -------------------------------------------------
